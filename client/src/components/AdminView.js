6b5dc405b9279a8828ffc0f2b574c72bdce64d2e
Panna <panna@Pannas-MacBook-Air.local>
2020-09-02 17:06:18 +0200

    finish primary animation

--- client/src/components/AdminView.js
+++ client/src/components/AdminView.js
@@ -33,28 +33,27 @@ class AdminView extends React.Component {
 
                 {this.props.quotes.map(quote => (
                     <div className="horizontal-1">
-                        <p style={{color: quote.color, backgroundColor: quote.backgroundColor}}>
+                        <p style={{ color: quote.color, backgroundColor: quote.backgroundColor }}>
                             {quote.text}
                         </p>
                         <button className="center" onClick={() => this.deleteQuote(quote)}>Delete</button>
                     </div>
                 ))}
-
-                {/* <br /> */}
+                
                 <h1>Add New Quote</h1>
 
                 <div className="horizontal-2">
                     <label>
                         Type Your Quotes Here
-                        <textarea ref={this.textRef}/>
+                        <textarea ref={this.textRef} />
                     </label>
                     <label>
                         Background Color
-                        <SketchPicker ref={this.backgroundColorRef}/>
+                        <SketchPicker ref={this.backgroundColorRef} />
                     </label>
                     <label>
                         Quotes Color
-                        <SketchPicker ref={this.colorRef}/>
+                        <SketchPicker ref={this.colorRef} />
                     </label>
                     <button onClick={() => this.addQuote()}>Add</button>
                 </div>
--- client/src/components/UserView.js
+++ client/src/components/UserView.js
@@ -14,19 +14,19 @@ class UserView extends React.Component {
             this.setState({
                 featuredQuoteIndex: nextIndex
             });
-        }, 3000);
+        }, 5000);
     }
 
     render() {
         const quote = this.props.quotes[this.state.featuredQuoteIndex];
         const backgroundStyle = { backgroundColor: quote ? quote.backgroundColor : "white" };
-        const quoteStyle = { color: quote.color };
+        const quoteStyle = { color: quote ? quote.color : "black" };
         return (
 
             <div className="UserView" style={backgroundStyle}>
                 <Link to="/admin" className="admin-link">admin</Link>
 
-                <h1 style={quoteStyle}>
+                <h1 className="quote" style={quoteStyle}>
                     {quote ? quote.text : "Please add quote(s) in admin view"}
                 </h1>
             </div>
