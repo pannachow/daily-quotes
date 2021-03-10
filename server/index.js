e511552e04c493f5a8ddc30eec56a48acbfa8fcb
pannachow <chowpanna@gmail.com>
2021-03-10 19:39:04 +0100

    undo config change

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
-            password: process.env.DB_PASS,
+            password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME || 'quotes',
         });
         ctx.connection = connection;
